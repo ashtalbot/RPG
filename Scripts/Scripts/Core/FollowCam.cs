00743fb237f9692717199b5602d03866abb28f49
Ash <23111810+ashtalbot@users.noreply.github.com>
2019-11-22 01:41:44 -0700

    Combat tweaks
    Combat and collision fixes.

---
+++ Scripts/Scripts/Core/FollowCam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    //public Transform playerObject;
+    //public float distanceFromObject = 35f;
+
+    
+    void Update()
+    {
+        transform.position = target.position;
+        //Vector3 lookOnObject = playerObject.position - transform.position;
+        //lookOnObject = playerObject.position - transform.position;
+        //transform.forward = lookOnObject.normalized;
+
+        //Vector3 playerLastPosition;
+        //playerLastPosition = playerObject.position - lookOnObject.normalized * distanceFromObject;
+        //playerLastPosition.y = playerObject.position.y + distanceFromObject / 2;
+        //transform.position = playerLastPosition;
+    }
+}
---
+++ Scripts/Scripts/Movement/Mover.cs
@@ -0,0 +1,59 @@
+﻿using RPG.Combat;
+using RPG.Core;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour, IAction
+    {
+        //[SerializeField] Transform target;
+
+        NavMeshAgent navMeshAgent;
+
+        private void Start()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+            
+
+        private void Update()
+        {
+            UpdateAnimator();
+        }
+
+
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);          
+            MoveTo(destination);
+        }
+
+
+        public void MoveTo(Vector3 destination)
+        {
+            navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
+        }
+
+        public void Cancel()
+        {
+            navMeshAgent.isStopped = true;
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = navMeshAgent.velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+
+        //Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+        //GetComponent<NavMeshAgent>().destination = target.position;
+
+       
+    }
+
+}
